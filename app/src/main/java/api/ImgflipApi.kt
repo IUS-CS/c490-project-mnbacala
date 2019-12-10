54dff09591121174ba124cc58ce86aa3cdfdbd8b
martinbacala <mnbacala@iu.edu>
2019-12-10 01:20:17 -0500

    updated functionality

--- app/src/main/java/api/ImgflipApi.kt
+++ app/src/main/java/api/ImgflipApi.kt
@@ -5,6 +5,7 @@ import com.c490.android.mockify.BuildConfig
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Url
 
 interface ImgflipApi {
@@ -15,5 +16,7 @@ interface ImgflipApi {
             "&format=json" +
             "&nojsoncallback=1" +
             "&extras=url_s")
-    fun fetchPhoto(): Call<ImgflipResponse>
+    fun fetchPhoto(@Url url: String): Call<ImgflipResponse>
+
+    //@POST
 }
--- app/src/main/java/com/c490/android/mockify/MainActivity.kt
+++ app/src/main/java/com/c490/android/mockify/MainActivity.kt
@@ -1,28 +1,69 @@
 package com.c490.android.mockify
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.getSystemService
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var button: Button
+    internal lateinit var editText: EditText
+    internal lateinit var text: TextView
+    internal lateinit var button: Button
     private lateinit var button2: Button
+    internal lateinit var input: String
+    internal lateinit var converted: String
+    internal lateinit var clipboard:ClipboardManager
+    internal lateinit var clip:ClipData
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.mockButton)
+        button2 = findViewById(R.id.copyButton)
+        editText = findViewById(R.id.enterText) as EditText
+        text = findViewById<TextView>(R.id.tConverted)
 
-        button = findViewById(R.id.button)
-        button2 = findViewById(R.id.button2)
-
-        button.setOnClickListener{
-
+        button.setOnClickListener {
+            Log.v("EditText", editText.getText().toString())
+            input = editText.getText().toString()
+            converted = mockMe(input)
+            text.setText(converted)
         }
-
+        copyButton.setOnClickListener {
+            clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val inputText:String
+            inputText = text.getText().toString()
+            clip = ClipData.newPlainText("Text",inputText)
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(getApplicationContext(), "Text Copied", Toast.LENGTH_SHORT).show()
+        }
     }
-
-
-
+    companion object{
+        fun mockMe(input: String): String {
+            var result = ""
+            for (i in input.indices) {
+                result += if ((i % 2 and i % 4 == 0)) {
+                    Character.toLowerCase(input[i])
+                } else Character.toUpperCase(
+                    input.get(i)
+                )
+            }
+            return result
+        }
+    }
 }
+
+
